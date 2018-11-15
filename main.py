3a027667ad630230f85be350e13616d86b9a2c22
oooolga <olga.xu823@gmail.com>
2018-11-15 18:20:25 -0500

    add updated codes

--- main.py
+++ main.py
@@ -1,11 +1,135 @@
 import torch
-import pdb
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 
-from .util.load_data import QuickDrawDataset
+use_cuda = torch.cuda.is_available()
+
+import pdb
+from tqdm import tqdm
+import numpy as np
+import argparse, os
+
+from util.load_data import load_quickdraw_data
+from util.model_util import save_checkpoint
+from models.vgg_model import Model
+
+state = {'train_loss': None,
+		 'valid_loss': None,
+		 'valid_accuracy': None}
+
+best_valid_loss = float('inf')
+
+model_path = 'saved_models/'
+
+def train(model, optimizer, train_loader):
+	model.train()
+
+	for i_batch, batch in tqdm(enumerate(train_loader)):
+
+		data, target = batch['image'].type(torch.FloatTensor), \
+					   batch['label'].type(torch.long)
+
+		if use_cuda:
+			data, target = data.cuda(), target.cuda()
+
+		output = model(data)
+		optimizer.zero_grad()
+		loss = F.cross_entropy(output, target)
+		loss.backward()
+		optimizer.step()
+
+def test(model, data_loader, mode='valid'):
+	model.eval()
+
+	loss_avg = 0.0
+	correct = 0
+
+	for i_batch, batch in tqdm(enumerate(data_loader)):
+
+		data, target = batch['image'].type(torch.FloatTensor), \
+					   batch['label'].type(torch.long)
+
+		if use_cuda:
+			data, target = data.cuda(), target.cuda()
+
+		output = model(data)
+		loss = F.cross_entropy(output, target)
+
+		pred = output.data.max(1)[1]
+		correct += float(pred.eq(target.data).sum())
+
+		loss_avg += float(loss)
+
+	state['{}_loss'.format(mode)] = loss_avg / len(data_loader)
+	state['{}_acc'.format(mode)] = correct / len(data_loader.dataset)
+
+
+def parse():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-lr', '--learning_rate', default=5e-3, type=float,
+						help='Learning rate.')
+	parser.add_argument('--batch_size', default=50, type=int,
+						help='Mini-batch size for training.')
+	parser.add_argument('--test_batch_size', default=200, type=int,
+						help='Mini-batch size for testing.')
+	parser.add_argument('--epochs', default=200, type=int,
+						help='Total number of epochs.')
+	parser.add_argument('--seed', default=123, type=int,
+						help='Random number seed.')
+	parser.add_argument('--weight_decay', default=1e-4, type=float, help='Weight decay')
+	parser.add_argument('--model_name', default='VGG', type=str, help='Model name')
+
+	args = parser.parse_args()
+	return args
 
 if __name__ == '__main__':
-	data = QuickDrawDataset()
+	# get arguments
+	args = parse()
 
-	l = len(data)
+	# set seeds
+	torch.manual_seed(args.seed)
+	if use_cuda:
+		torch.cuda.manual_seed_all(args.seed)
 
-	pdb.set_trace()
+	train_loader, valid_loader, test_loader, unique_labels = load_quickdraw_data(batch_size=args.batch_size,
+																test_batch_size=args.test_batch_size)
+
+	model = Model()
+	if use_cuda:
+		model.cuda()
+
+	model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+	params = sum([np.prod(p.size()) for p in model_parameters])
+	print('Total number of parameters: {}\n'.format(params))
+
+	optimizer = optim.Adam(params=model.parameters(), lr=args.learning_rate,
+						   eps=1e-07,
+						   weight_decay=args.weight_decay)
+
+	scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
+
+	epoch_start = 0
+
+	for epoch_i in range(epoch_start, args.epochs+1):
+		print('|\tEpoch {}/{}:'.format(epoch_i, args.epochs))
+		scheduler.step()
+
+		if epoch_i != 0:
+			train(model, optimizer, train_loader)
+
+		test(model, valid_loader)
+
+		print('|\t\t[Valid]:\taccuracy={:.3f}\tloss={:.3f}'.format(state['valid_acc'],
+																   state['valid_loss']))
+
+
+		if state['valid_loss'] < best_valid_loss:
+			best_valid_loss = state['valid_loss']
+			save_checkpoint({
+				'epoch_i': epoch_i,
+				'state_dict': model.state_dict(),
+				'optimizer': optimizer.state_dict(),
+				'best_loss': best_valid_loss,
+				'unique_labels': unique_labels
+				}, os.path.join(model_path, args.model_name+'.pt'))
--- util/load_data.py
+++ util/load_data.py
@@ -1,4 +1,4 @@
-import panda as pd
+import pandas as pd
 import numpy as np
 import pdb
 
@@ -9,26 +9,69 @@ class QuickDrawDataset(Dataset):
 
 	def __init__(self, train_im_path='./data/train_images.npy',
 				 test_im_path='./data/test_images.npy',
-				 train_label_path = './data/train_labels.csv'):
+				 train_label_path = './data/train_labels.csv',
+				 unique_labels = None,
+				 mode='train'):
 
 		self.train_im_path = train_im_path
 		self.test_im_path = test_im_path
 		self.train_label_path = train_label_path
 
-		self.train_im = np.load(self.train_im_path)
-		self.test_im = np.load(self.test_im_path)
-		self.train_label = pd.read_csv(self.train_label_path)
-		self.mode = 'train'
+		if mode == 'train' or mode == 'valid':
+			train_im = np.load(self.train_im_path, encoding='bytes')
+			train_im = np.concatenate(train_im[:,1]).reshape((-1,1,100,100))
+			total_train = int(len(train_im)*0.9)
+			train_labels_df = pd.read_csv(self.train_label_path)
+			train_labels = self.set_numeric_labels(train_labels_df, unique_labels)
 
-	def set_train_mode(self):
-		self.mode = 'train'
+			if mode == 'train':
+				self.operating_im = train_im[:total_train]
+				self.operating_labels = train_labels[:total_train]
+			else:
+				self.operating_im = train_im[total_train:]
+				self.operating_labels = train_labels[total_train:]
 
-	def set_validation_mode(self):
-		self.mode = 'valid'
+		elif mode == 'test':
+			test_im = np.load(self.test_im_path, encoding='bytes')
+			self.operating_im = np.concatenate(test_im[:,1]).reshape((-1,1,100,100))
+			self.operating_labels = None
 
-	def set_test_mode(self):
-		self.mode = 'test'
+
+	def set_numeric_labels(self, labels_df, unique_labels):
+
+		for i in range(len(unique_labels)):
+			labels_df.loc[labels_df.Category==unique_labels[i],'Category'] = i
+		return labels_df.values[:,1]
 
 	def __len__(self):
 
-		pdb.set_trace()
+		return len(self.operating_im)
+
+	def __getitem__(self, idx):
+		if self.operating_labels is None:
+			curr_label = None
+		else:
+			curr_label = self.operating_labels[idx]
+
+		return {'image': self.operating_im[idx], 'label': curr_label}
+
+
+def load_quickdraw_data(batch_size=50, test_batch_size=1000, 
+						train_im_path='./data/train_images.npy',
+						test_im_path='./data/test_images.npy',
+						train_label_path = './data/train_labels.csv',
+						unique_labels=None):
+	if unique_labels is None:
+		labels_df = pd.read_csv(train_label_path)
+		unique_labels = labels_df.Category.unique()
+
+	train_quickdraw_dataset = QuickDrawDataset(mode='train', unique_labels=unique_labels)
+	train_loader = DataLoader(train_quickdraw_dataset, batch_size=batch_size, shuffle=True)
+
+	valid_quickdraw_dataset = QuickDrawDataset(mode='valid', unique_labels=unique_labels)
+	valid_loader = DataLoader(valid_quickdraw_dataset, batch_size=test_batch_size)
+
+	test_quickdraw_dataset = QuickDrawDataset(mode='test')
+	test_loader = DataLoader(test_quickdraw_dataset, batch_size=test_batch_size)
+
+	return train_loader, valid_loader, test_loader, unique_labels
